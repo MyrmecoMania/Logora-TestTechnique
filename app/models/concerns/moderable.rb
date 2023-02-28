35a4504f7ecf4f922808f68d84630996bb0b9cdf
eliott norie <eliott.norie@gmail.com>
2023-02-28 17:21:12 +0100

    fixed nombre d'arguments de la fonction moderator

--- app/models/concerns/moderable.rb
+++ app/models/concerns/moderable.rb
@@ -5,19 +5,21 @@ module Moderable
 
   included do
     # Une fonction qui modére uniquement l'instance d'une classe contenant le concern Moderable (méthode d'instance)
-    def moderator(*args)
+    def moderator(arg1,*args)
+      args.push(arg1)
       args.each do |arg|
         uri = URI("https://moderation.logora.fr/predict?text=#{self.send(arg)}")
         response = JSON.parse(Net::HTTP.get(uri))["prediction"]["0"]
         self.update(is_accepted: false) if response > 0.95
       end
-      self.update(is_accepted: true) if self.is_accepted != false
+      self.update(is_accepted: true) if self.is_accepted == nil
     end
   end
 
   class_methods do
     # Une fonction qui modére toutes les instances d'une classe contenant le concern Moderable (méthode de classe)
-    def moderator(*args)
+    def moderator(arg1,*args)
+      args.push(arg1)
       where(is_accepted: nil).each { |instance| instance.moderator(args[0]) }
     end
   end
--- test/models/moderated_model_test.rb
+++ test/models/moderated_model_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class ModeratedModelTest < ActiveSupport::TestCase
-  test "ModeratedModel a bien la méthode d'instance moderator" do
-    modelmoderator = ModelModerator.new
-    assert modelmoderator.moderator
+  test "Les instances de la classe ModeratedModel ont bien la méthode moderator qui donne un booléen a la colonne is_accepted" do
+    modelmoderator = ModeratedModel.new(title: "Obligation vaccinale chez les enfants", content: "Je ne pense pas qu'il soit nécessaire")
+    puts "L'instance crée :
+    titre: #{modelmoderator.title}
+    contenu: #{modelmoderator.content}
+    status: #{modelmoderator.is_accepted}"
+    puts "Utilisons la méthode d'instance moderator"
   end
 end
